36af106bc28ad6117d9c25daabf8cfb9d637a291
M1zyuk1 <me@mizune.ms>
2021-12-07 19:39:57 +0900

    Fix navigation bar color condition

--- app/src/main/java/ms/mizune/edgetoedgesample/MainActivity.kt
+++ app/src/main/java/ms/mizune/edgetoedgesample/MainActivity.kt
@@ -1,12 +1,11 @@
 package ms.mizune.edgetoedgesample
 
+import android.content.res.Resources
 import android.os.Build
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
+import androidx.core.view.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import ms.mizune.edgetoedgesample.databinding.ActivityMainBinding
 import ms.mizune.edgetoedgesample.models.ListViewItem
@@ -23,11 +22,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.navigationBarColor = getNavigationBarColorInt()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initRecyclerView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        window.navigationBarColor = getNavigationBarColorInt()
         adjustPadding()
+        adjustMargin()
     }
 
     private fun initRecyclerView() {
@@ -44,11 +48,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getNavigationBarColorInt() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        getTransparentNavigationBarColor()
-    } else {
-        getTranslucentNavigationBarColor()
+    private fun adjustMargin() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.fab) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                rightMargin = insets.right + FAB_MARGIN
+                bottomMargin = insets.bottom + FAB_MARGIN
+            }
+
+            windowInsets
+        }
     }
+
+    private fun getNavigationBarColorInt() =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q || isGestureNavigationEnabled()) {
+            getTransparentNavigationBarColor()
+        } else {
+            getTranslucentNavigationBarColor()
+        }
 
     private fun getTransparentNavigationBarColor(): Int {
         return getColor(android.R.color.transparent)
@@ -56,6 +73,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun getTranslucentNavigationBarColor(): Int {
         return getColor(R.color.translucent)
+    }
+
+    private fun isGestureNavigationEnabled(): Boolean {
+        val gestureModeInt = 2
+
+        val navigationModeResId =
+            resources.getIdentifier("config_navBar_interactionMode", "integer", "android")
+                .takeIf { it > 0 }
+
+        val navigationModeInt = try {
+            navigationModeResId?.let(resources::getInteger)
+        } catch (_: Resources.NotFoundException) {
+            null
+        }
+
+        return navigationModeInt == gestureModeInt
     }
 
     companion object {
@@ -66,5 +99,7 @@ class MainActivity : AppCompatActivity() {
                 R.drawable.ic_launcher_background
             )
         }
+
+        const val FAB_MARGIN = 24
     }
 }
